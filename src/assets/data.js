7cee3a674faf99ad9a3c2bd0bb272a74e51dc53c
Shivam Prajapati <shivam.prajapati.phe21@itbhu.ac.in>
2024-02-28 00:00:11 +0530

    jai ho

---
+++ src/assets/data.js
@@ -0,0 +1,58 @@
+
+const data = [
+    {
+        id: "1",
+        img1: "./assets/image1.svg",
+        heading: "WixPro 72-Inch Responsive Website Builder",
+        text1:
+            "- Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
+        text2:
+            "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
+        rating: "9.8",
+        remark: "exceptional",
+        star: "./assets/image2.svg",
+        offer: "Best choice",
+    },
+    {
+        id: "2",
+        img1: "./assets/image1.svg",
+        heading: "SiteCraft 68-Inch Ultimate Web Design Studio",
+        text1:
+            "- Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)",
+        text2:
+            "[What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integrations.",
+        rating: "9.5",
+        remark: "excellent",
+        star: "./assets/image3.svg",
+        offer: "Best Value",
+    },
+    {
+        id: "3",
+        img1: "./assets/image1.svg",
+        heading: "WixPro 72-Inch Responsive Website Builder",
+        text1:
+            "- Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
+        text2:
+            "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
+        rating: "9.8",
+        remark: "exceptional",
+        star: "./assets/image2.svg",
+        offer: "",
+    },
+    {
+        id: "4",
+        img1: "./assets/image1.svg",
+        heading: "SiteCraft 68-Inch Ultimate Web Design Studio",
+        text1:
+            "- Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)",
+        text2:
+            "[What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integrations.",
+        rating: "9.5",
+        remark: "exceptional",
+        star: "./assets/image3.svg",
+        offer: "",
+    },
+];
+
+
+export default data;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -11,65 +11,12 @@ import { useMediaQuery } from "react-responsive";
 import { BsCaretDownFill } from "react-icons/bs";
 import { BsCaretUpFill } from "react-icons/bs";
 import { BsCaretRightFill } from "react-icons/bs";
+import data from "../assets/data";
 
 const Home = () => {
   const isMobile = useMediaQuery({ maxWidth: 820 });
   const [showLinks, setShowLinks] = useState(false);
 
-  const data = [
-    {
-      id: "1",
-      img1: "./assets/image1.svg",
-      heading: "WixPro 72-Inch Responsive Website Builder",
-      text1:
-        "- Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
-      text2:
-        "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
-      rating: "9.8",
-      remark: "exceptional",
-      star: "./assets/image2.svg",
-      offer: "Best choice",
-    },
-    {
-      id: "2",
-      img1: "./assets/image1.svg",
-      heading: "SiteCraft 68-Inch Ultimate Web Design Studio",
-      text1:
-        "- Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)",
-      text2:
-        "[What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integrations.",
-      rating: "9.5",
-      remark: "excellent",
-      star: "./assets/image3.svg",
-      offer: "Best Value",
-    },
-    {
-      id: "3",
-      img1: "./assets/image1.svg",
-      heading: "WixPro 72-Inch Responsive Website Builder",
-      text1:
-        "- Comprehensive Digital Platform Creation Tool, Streamlined Design Interface for Professional Websites and Online Stores (Black/Blue)",
-      text2:
-        "[What You Get]: Receive the WixPro website builder suite, access to premium design templates, an extensive library of widgets and apps, and detailed step-by-step guides.",
-      rating: "9.8",
-      remark: "exceptional",
-      star: "./assets/image2.svg",
-      offer: "",
-    },
-    {
-      id: "4",
-      img1: "./assets/image1.svg",
-      heading: "SiteCraft 68-Inch Ultimate Web Design Studio",
-      text1:
-        "- Advanced Site Creation Toolkit, Intuitive Drag-and-Drop Editor for Dynamic Websites and E-commerce Platforms (Green/White)",
-      text2:
-        "[What You Get]: Gain access to the SiteCraft design studio, featuring a robust selection of design elements, SEO optimization tools, and e-commerce integrations.",
-      rating: "9.5",
-      remark: "exceptional",
-      star: "./assets/image3.svg",
-      offer: "",
-    },
-  ];
 
   const handleLinks = () => {
     setShowLinks((prev) => !prev);
